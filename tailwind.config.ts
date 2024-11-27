5d3ca6fa7c3974e5a9c36b5026471e0833d8dcac
Santam Kumai <santtamk@gmail.com>
2024-11-27 21:06:08 +0530

    made it switchable between dark mode and light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+        primary: "#FAED26",
+        secondary: "#46344E",
+        tertiary: {
+          dark: "#F27405",
+          light: "#F2C641",
+        },
       },
     },
   },
