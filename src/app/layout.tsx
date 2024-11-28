50c9039f1a34ec63d1ac5172be09305bf51ef7f9
Santam Kumai <santtamk@gmail.com>
2024-11-28 21:40:54 +0530

    creating form data on auth page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
-import ThemeProvider from "./components/ThemeProvider/ThemeProvider";
+import Header from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import ThemeProvider from "../components/ThemeProvider/ThemeProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
